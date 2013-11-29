0df40a0d26a85363bfda7beb6aab3ab0f0e430b5
root <root@vm126.0x50.net>
2013-11-29 15:47:47 +0000

    fix launch

--- launch.php
+++ launch.php
@@ -27,8 +27,9 @@ $NB_CPU=$infosystem['CPU_NB'];
 $PWD=getcwd();
 
 $CONF_ROUTINE = array(
-//	'CPU_SHA256' => '/root/zenbench_infosrv/zenbench_go_cpu_mono_sha256 -o #OUTPUT#'
-	'CPU_WPAPSK_JOHN'=>$PWD.'/john-1.7.2-bp17-mpi8/run.sh '.intval(1 + $NB_CPU * 2).' #OUTPUT#'
+	'CPU_SHA256_MONO' => $PWD.'/cpu_mono_sha256/main -d 10s -o #OUTPUT#',
+	'CPU_SHA256_MULTI' => $PWD.'/cpu_multi_sha256/main -t '.intval(1 + $NB_CPU *2 ).' -d 10s -o #OUTPUT#',
+	'CPU_WPAPSK_JOHN' => $PWD.'/john-1.7.2-bp17-mpi8/run.sh '.intval(1 + $NB_CPU * 2).' #OUTPUT#'
 );
 
 $JSON_TEMPLATE='{
